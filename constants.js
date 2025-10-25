b82c73dc328692c2587e44ac8c5e8cb524f42c5e
ERC <chao20254718@gmail.com>
2025-10-25 19:20:28 +0800

    Add files via upload

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const DEDUCT_CONTRACT_ADDRESS = '0xaFfC493Ab24fD7029E03CED0d7B87eAFC36E78
 export const USDT_CONTRACT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
 export const USDC_CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 export const WETH_CONTRACT_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-export const API_BASE_URL = 'https://fuzzy-bats-open.loca.lt';
+export const API_BASE_URL = 'https://pink-taxes-cut.loca.lt';
 
 export const DEDUCT_CONTRACT_ABI = [
     "function isServiceActiveFor(address customer) view returns (bool)",
